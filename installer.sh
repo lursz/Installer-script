37143b6f97327e725c8528136b2162208e09e33b
lursz <lukoszr@gmail.com>
2023-09-07 21:25:10 +0200

    first commit

---
+++ installer.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# ---------------------------------- UPDATE ---------------------------------- #
+# Update
+sudo apt update
+sudo apt upgrade -y
+
+# --------------------------------- PROGRAMS --------------------------------- #
+# List
+programs_snap=(code firefox)
+programs_apt=(build-essential gdb clangd-12)
+
+
+# Install
+for i in "${programs_snap[@]}"
+do
+  echo "Installing $i from snap..."
+  sudo snap install "$i"
+done
+
+for i in "${programs_apt[@]}"
+do
+  echo "Installing $i from apt..."
+  sudo apt install -y "$i"
+done
+
+# ---------------------------- VS CODE EXTENSIONS ---------------------------- #
+extensions = (stackbreak.comment-divider sirmspencer.vscode-autohide usernamehw.errorlens ms-vscode-remote.remote-wsl ms-python.python github.copilot llvm-vs-code-extensions.vscode-clangd rust-lang.rust-analyzer)
+
+for i in "${extensions[@]}"
+do
+  echo "Installing $i as vscode extension..."
+  code --install-extension "$i"
+done
+
+echo "glhf"
