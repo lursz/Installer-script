60d09dd32132c0a30e6b142525edf1c2e5584f31
lursz <lukoszr@gmail.com>
2023-09-18 00:16:50 +0200

    added guide

--- windows_script.ps1
+++ windows_script.ps1
@@ -1,8 +1,12 @@
-# List                                           vscode    
-$programs = "brave", "spotify", "discord", "XP9KHM4BK9FZ7Q" 
+# List          vscode    
+$programs = "XP9KHM4BK9FZ7Q", "brave", "spotify", "discord", "gimp"
 
 # Install (apps from the Microsoft Store)
 foreach ($i in $programs) {
   winget install $i --source=msstore --silent --accept-package-agreements --accept-source-agreements
   Write-Host "$i is installed."
 }
+
+
+# Install wsl extension for vscode
+code --install-extension "ms-vscode-remote.remote-wsl"
